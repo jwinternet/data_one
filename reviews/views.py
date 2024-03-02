2ddc712ce413351538f500c51fa59b35a66d182c
Jared Winter <jwinternet@outlook.com>
2024-03-02 14:36:41 -0500

    Somewhere in Chapter 2...I think?

---
+++ reviews/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
+from .utils import average_rating
+
+
+def index(request):
+    return render(request, "base.html")
+
+
+def book_search(request):
+    search_text = request.GET.get("search", "")
+    return render(
+        request,
+        "reviews/search-results.html",
+        {"search_text": search_text}
+    )
